a2ba5718a15743eb77cf866659f316423697d249
Adnan <68817284+AdnanH1006@users.noreply.github.com>
2022-04-23 21:50:13 +0600

    Created a MainNavigation and a Layout (Wrapper) component | Used the NavLink component inside the MainNavigation component | Wrapped the Layout component around all the Route definitions in the App.js page

--- src/App.js
+++ src/App.js
@@ -3,18 +3,21 @@ import AllQuotes from "./pages/AllQuotes";
 import NewQuotes from "./pages/NewQuote";
 import QuoteDetail from "./pages/QuoteDetail";
 import Comments from "./components/comments/Comments";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Navigate replace to="/quotes" />} />
-        <Route path="/quotes" element={<AllQuotes />} />
-        <Route path="/quotes/:quoteId/*" element={<QuoteDetail />}>
-          <Route path="comments" element={<Comments />} />
-        </Route>
-        <Route path="/new-quote" element={<NewQuotes />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Navigate replace to="/quotes" />} />
+          <Route path="/quotes" element={<AllQuotes />} />
+          <Route path="/quotes/:quoteId/*" element={<QuoteDetail />}>
+            <Route path="comments" element={<Comments />} />
+          </Route>
+          <Route path="/new-quote" element={<NewQuotes />} />
+        </Routes>
+      </Layout>
     </>
   );
 }
