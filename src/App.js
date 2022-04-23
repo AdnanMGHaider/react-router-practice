9c21c7b6c7c1ad182e503c610bd91b69e14ec65e
Adnan <68817284+AdnanH1006@users.noreply.github.com>
2022-04-23 20:46:24 +0600

    Used the Navigate component in App.js | Used the useParams() hook in the QuoteDetail page to extract the dynamic segment of the url and display it on the screen

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import AllQuotes from "./pages/AllQuotes";
-import NewQuotes from "./pages/NewQuotes";
+import NewQuotes from "./pages/NewQuote";
 import QuoteDetail from "./pages/QuoteDetail";
 
 function App() {
   return (
     <>
       <Routes>
+        <Route path="/" element={<Navigate replace to="/quotes" />} />
         <Route path="/quotes" element={<AllQuotes />} />
         <Route path="/quotes/:quoteId" element={<QuoteDetail />} />
         <Route path="/new-quote" element={<NewQuotes />} />
--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,8 +1,14 @@
-import React from "react";
+import { useParams } from "react-router-dom";
 
 function QuoteDetail() {
-  return <h1>Quote Detail Page</h1>;
-}
+  const params = useParams();
+
+  return (
+    <>
+      <h1>Quote Detail Page</h1>
+      <p>{params.quoteId}</p>
+    </>
+  );
 }
 
 export default QuoteDetail;
