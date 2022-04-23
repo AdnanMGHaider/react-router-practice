d1a38650e05c7fa3ad808edf4f929a19b06b036d
Adnan <68817284+AdnanH1006@users.noreply.github.com>
2022-04-23 22:01:05 +0600

    Added some dummy data into the AllQuotes page/component | Imported the pre-made QuoteList component into the AllQuotes page and passed the dummy data as props into it

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,7 +1,12 @@
-import React from "react";
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Adnan", text: "Learning React Router is fun" },
+  { id: "q2", author: "Steve", text: "Web development is amazing" },
+];
 
 function AllQuotes() {
-  return <h1>All Quotes Page</h1>;
+  return <QuoteList quotes={DUMMY_QUOTES} />;
 }
 
 export default AllQuotes;
