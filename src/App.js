0e07feb5ea72ee8896e3453bcef44da20d1448b7
Adnan <68817284+AdnanH1006@users.noreply.github.com>
2022-04-23 20:28:03 +0600

    Used BrowserRouter in index.js | Created AllQuotes, NewQuote and QuoteDetail pages | Used the Routes and Route components inside App.js to register new routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import AllQuotes from "./pages/AllQuotes";
+import NewQuotes from "./pages/NewQuotes";
+import QuoteDetail from "./pages/QuoteDetail";
+
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/quotes" element={<AllQuotes />} />
+        <Route path="/quotes/:quoteId" element={<QuoteDetail />} />
+        <Route path="/new-quote" element={<NewQuotes />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/QuoteDetail.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function QuoteDetail() {
+  return <h1>Quote Detail Page</h1>;
+}
+}
+
+export default QuoteDetail;
