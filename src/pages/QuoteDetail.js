8e861eec277712fa65c24113b550762460eee343
Adnan <68817284+AdnanH1006@users.noreply.github.com>
2022-04-24 13:39:05 +0600

    Used the Link component from react router inside the pre-made QuoteItem component | Imported pre-made HighlightedQuote component into QuoteDetail page and added code to display quote details as per the dynamic segement on the url

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,12 +1,24 @@
 import { useParams, Outlet } from "react-router-dom";
 
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Adnan", text: "Learning React Router is fun" },
+  { id: "q2", author: "Steve", text: "Web development is amazing" },
+];
+
 function QuoteDetail() {
   const params = useParams();
 
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No quote found!</p>;
+  }
+
   return (
     <>
-      <h1>Quote Detail Page</h1>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Outlet />
     </>
   );
