be7a8224ae41f069a10384d228043e5e451399bc
Adnan <68817284+AdnanH1006@users.noreply.github.com>
2022-04-25 13:17:28 +0600

    Used the Link component in App.js to create a link to view the comments nested route

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate, Link } from "react-router-dom";
 import AllQuotes from "./pages/AllQuotes";
 import NewQuotes from "./pages/NewQuote";
 import QuoteDetail from "./pages/QuoteDetail";
@@ -8,19 +8,27 @@ import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Navigate replace to="/quotes" />} />
-          <Route path="/quotes" element={<AllQuotes />} />
-          <Route path="/quotes/:quoteId/*" element={<QuoteDetail />}>
-            <Route path="comments" element={<Comments />} />
-          </Route>
-          <Route path="/new-quote" element={<NewQuotes />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Layout>
-    </>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Navigate replace to="/quotes" />} />
+        <Route path="/quotes" element={<AllQuotes />} />
+        <Route path="/quotes/:quoteId/*" element={<QuoteDetail />}>
+          <Route
+            path=""
+            element={
+              <div className="centered">
+                <Link className="btn--flat" to={`comments`}>
+                  Load Comments
+                </Link>
+              </div>
+            }
+          />
+          <Route path={`comments`} element={<Comments />} />
+        </Route>
+        <Route path="/new-quote" element={<NewQuotes />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Layout>
   );
 }
 
